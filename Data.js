142ba9ee85024d47891fb9e694b7de476559132c
tian000 <tian543@gmail.com>
2015-12-22 01:00:22 -0800

    adding plot.ly

--- Data.js
+++ Data.js
@@ -18,6 +18,7 @@ function facebookDictionaryToMatrix(fb_dict) {
     console.log(JSON.stringify(data));
 
     visualizeMatrix(data, names, "myDiv");
+    displayClusters(data,names,"clusterOutput");
     return data;
 }
 
@@ -32,4 +33,52 @@ function visualizeMatrix(matrix, labels, div_id){
     ];
 
     Plotly.newPlot(div_id, data);
+}
+
+function displayClusters(matrix, labels, div_id){
+    var edge_data = [];
+    for (var i in labels) {
+        for (var j in labels) {
+            if(matrix[i][j] != null) {
+                edge_data.push({source: labels[i], target: labels[j], weight: 1/matrix[i][j]});
+            }
+        }
+    }
+
+    var community = jLouvain().nodes(labels).edges(edge_data);
+
+    var result = community();
+
+    var bins = {};
+    var max_bin = 0;
+    for(var name in result){
+        if(result[name] > max_bin){
+            max_bin = result[name];
+        }
+        if(!(result[name] in bins)){
+            bins[result[name]] = [name];
+        }else{
+            bins[result[name]].push(name);
+        }
+    }
+
+    var list = document.createElement("UL");
+    for(var i = 0; i<=max_bin; i++){
+        var list_group_node = document.createElement("LI");
+        list_group_node.appendChild(document.createTextNode("Cluster " + i));
+
+        var group_list = document.createElement("UL");
+
+        for(var j in bins[i]){
+            var temp_list_item = document.createElement("LI");
+            temp_list_item.appendChild(document.createTextNode(bins[i][j]));
+            group_list.appendChild(temp_list_item);
+        }
+
+        list_group_node.appendChild(group_list);
+        list.appendChild(list_group_node);
+    }
+
+    document.getElementById(div_id).appendChild(list);
+
 }
