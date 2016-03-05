c0995c1bf71c82b97f54a4f1d13422085647ac23
tian000 <nu4@rice.edu>
2016-03-05 17:09:06 -0600

    php commit

--- data_collector.php
+++ data_collector.php
@@ -6,6 +6,19 @@ echo $_POST["anon_id"];
 echo $_POST["birthday"];
 echo $_POST["friend_count"];
 echo $_POST["anon_data"];
-phpinfo();
+
+$servername = "localhost";
+$username = "root";
+$password = "marie_napas";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
 ?>
 </body>
